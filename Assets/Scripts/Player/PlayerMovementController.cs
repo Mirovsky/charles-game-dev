d1fad59f705c2fc43af590f52d610dd5f7c93e2f
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-10-27 18:30:04 +0100

    add jumping / grounded mechanic with multi jump option

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 
@@ -8,20 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
     [Header("Move Settings")]
-    [SerializeField]
-    float moveSpeed;
+    [SerializeField] float moveSpeed = default;
     [Space]
     [Header("Jump Settings")]
-    [SerializeField]
-    float jumpHeight;
-    [SerializeField]
-    float timeToJumpApex;
+    [SerializeField] float jumpHeight = default;
+    [SerializeField] float timeToJumpApex = default;
+    [SerializeField] bool enableMultiJump = default;
+    [SerializeField] int multiJumpCount = 2;
     [Space]
     [Header("Dampening")]
-    [SerializeField]
-    float groundedDampen;
-    [SerializeField]
-    float airbornDampen;
+    [SerializeField] float groundedDampen = default;
+    [SerializeField] float airbornDampen = default;
+    [Space]
+    [Header("BorderChecks")]
+    [SerializeField] Transform aboveCheck = default;
+    [SerializeField] Transform rightCheck = default;
+    [SerializeField] Transform belowCheck = default;
+    [SerializeField] Transform leftCheck = default;
+    
+    [Header("Debug Info")]
+    [SerializeField] CollisionChecks collisions;
 
     PlayerInputActions playerInput;
 
@@ -30,6 +34,15 @@ public class PlayerMovementController : MonoBehaviour
     float gravity;
     float jumpVelocity;
     float moveSmoothing;
+
+    int jumpCount = 0;
+
+    [System.Serializable]
+    public struct CollisionChecks
+    {
+        public bool above, below;
+        public bool left, right;
+    }
 
     void OnEnable() => playerInput.Enable();
     void OnDisable() => playerInput.Disable();
@@ -49,12 +62,19 @@ public class PlayerMovementController : MonoBehaviour
 
     void FixedUpdate()
     {
+        HandleCollisions();
+
         var moveDirection = playerInput.Player.Movement.ReadValue<float>();
 
         float targetXVelocity = moveDirection * moveSpeed * Time.deltaTime;
 
         var velocity = rb.velocity;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetXVelocity, ref moveSmoothing, groundedDampen);
+        velocity.x = Mathf.SmoothDamp(
+            velocity.x,
+            targetXVelocity,
+            ref moveSmoothing,
+            collisions.below ? groundedDampen : airbornDampen
+        );
         velocity.y += gravity * Time.deltaTime;
         rb.velocity = velocity;
     }
@@ -62,7 +82,38 @@ public class PlayerMovementController : MonoBehaviour
     void OnJump()
     {
         var velocity = rb.velocity;
-        velocity.y = jumpVelocity;
+        
+        if (collisions.below || (enableMultiJump && jumpCount < multiJumpCount)) {
+            velocity.y = jumpVelocity;
+
+            jumpCount += 1;
+        }
+
         rb.velocity = velocity;
     }
+
+    void HandleCollisions()
+    {
+        var prev = collisions;
+
+        collisions.above = Physics.Linecast(transform.position, aboveCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        collisions.below = Physics.Linecast(transform.position, belowCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        collisions.left = Physics.Linecast(transform.position, leftCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        collisions.right = Physics.Linecast(transform.position, rightCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    
+        if (!prev.below && collisions.below) HitBelow();
+        if (!prev.above && collisions.above) HitAbove();
+        if (!prev.left && collisions.left) HitLeft();
+        if (!prev.right && collisions.right) HitRight();
+    }
+
+    void HitBelow() {
+        jumpCount = 0;
+    }
+
+    void HitAbove() {}
+    
+    void HitLeft() {}
+    
+    void HitRight() {}
 }
