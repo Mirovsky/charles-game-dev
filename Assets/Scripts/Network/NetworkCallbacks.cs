806b4562d3868ca9d003bbc84371ffea61a4453e
michona <mitevski1201miki@gmail.com>
2019-11-02 13:58:54 +0100

    Adds initial bold assets

---
+++ Assets/Scripts/Network/NetworkCallbacks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
