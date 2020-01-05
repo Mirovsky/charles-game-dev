a23e7f8cf016774a83e3f0939bcae97c0eaebbc9
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-01-05 23:44:49 +0100

    add ingame UI prefabs for mobile and VR player

---
+++ Assets/Scripts/Menu/LevelPlay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+
+public class LevelPlay : MonoBehaviour
+{
+    [SerializeField]
+    LevelScriptableObject level;
+
+    [Header("UI")]
+    [SerializeField]
+    Button playButton;
+    [SerializeField]
+    GameObject locked;
+
+    [Header("")]
+    [SerializeField]
+    LevelSelectController levelSelectController;
+
+
+    void Awake()
+    {
+        var isLocked = Persistence.IsLevelLocked(level.levelIdentifier);
+
+        locked.SetActive(false);
+        // playButton.interactable = !isLocked;
+
+        // if (!isLocked)
+            playButton.onClick.AddListener(PlayLevel);
+    }
+
+    void PlayLevel()
+    {
+        levelSelectController.PlayLevel(level);
+    }
+}
---
+++ Assets/Scripts/Menu/LevelSelectController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Mirror;
+
+
+public class LevelSelectController : MonoBehaviour
+{
+    [SerializeField]
+    NetworkManager networkManager;
+    [SerializeField]
+    EventSystem eventSystem;
+    
+
+    public void PlayLevel(LevelScriptableObject level)
+    {
+        eventSystem.enabled = false;
+        networkManager.ServerChangeScene(level.scenePath);
+    }
+}
