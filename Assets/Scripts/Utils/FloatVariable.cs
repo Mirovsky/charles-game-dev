3d61cc26a8e4701f0f5b50e85610bf8928eab4df
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-01-07 17:50:47 +0100

    add translate and scale for VR environment

---
+++ Assets/Scripts/Utils/FloatVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "OOO/Variables/Float")]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+
+    public static implicit operator float(FloatVariable f) => f.value;
+}
