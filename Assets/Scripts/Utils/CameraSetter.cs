2c31909c4a46508ec62131a3be66f5db1ec8b8c8
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-02-19 18:20:14 +0100

    Update input system library

---
+++ Assets/Scripts/Utils/CameraSetter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class CameraSetter : MonoBehaviour
+{
+    void Start()
+    {
+        var canvas = GetComponent<Canvas>();
+
+        canvas.worldCamera = Camera.main;
+        canvas.planeDistance = 4;
+    }
+}
